c5d7772e958b446143a003096a50ca93b50b1b56
SanskritFritz <>
2026-03-23 23:14:37 +0100

    Introducing --csv=<path/to/output.csv>

--- Track-n-Graph_to_Obsidian.fish
+++ Track-n-Graph_to_Obsidian.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-# Import [Track & Graph](https://github.com/SamAmco/track-and-graph) data to Obsidian.
+# Import [Track & Graph](https://github.com/SamAmco/track-and-graph) data to Obsidian or CSV file.
 # Generate notes with properties, one for each data point.
 # File name is the datetime of a data point.
 # Properties set:
@@ -10,6 +10,10 @@
 #     TnG_Label(text)
 #     TnG_Note(text)
 #
+# Alternatively a csv file can be written instead of Obsidian,
+# which then can be imported into Obsidian via its import plugin.
+# This is recommended for an initial mass import, since it's **much** faster.
+#
 # For usage: --help
 # Enjoy!
 #     SanskritFritz (gmail)
@@ -17,7 +21,7 @@
 # TODO also filter via label and note
 #      error detection for Obsidian calls
 
-argparse 'd/database=' 't/tracker=+' 'min-date=' 'max-date=' 'dry-run' 'h/help'  -- $argv
+argparse 'd/database=' 't/tracker=+' 'min-date=' 'max-date=' 'dry-run' 'h/help' 'csv=' -- $argv
 or return
 
 if test $_flag_help
@@ -26,6 +30,7 @@ if test $_flag_help
 	echo "    [-d|--database=</absolute/path/to/TrackAndGraphBackup.db>] \\"
 	echo "    [-t|--tracker=<tracker> [...]] \\"
 	echo "    [--min-date=<YYYY-MM-DD>] [--max-date=<YYYY-MM-DD>] \\"
+	echo "    [--csv=<path/to/output.csv>]"
 	echo "    [--dry-run] \\"
 	echo "    [-h|--help]"
 	echo
@@ -39,6 +44,9 @@ if test $_flag_help
 	echo
 	echo "Obsidian path is determined by the property 'TnG_ROOT: true'"
 	echo "or defaults to 'Data/Track-n-Graph'"
+	echo
+	echo "If the option --csv=<path/to/output.csv> is given,"
+	echo "then ONLY the csv file will be written."
 	echo
 	return
 end
@@ -118,21 +126,30 @@ if test $_flag_dry_run
 	echo $SQLquery
 end;
 
-set NotePath $DefaultPath
-set RootNote (obsidian search query='["TnG_ROOT":true]' | string collect)
-if test (echo $RootNote | wc -l) -gt 1
-	echo 'Error: ["TnG_ROOT":true] defined more than once in the vault.'
-	return 1
-end
-if test $RootNote != "No matches found."
-	set NotePath (dirname $RootNote)
-end
+if test $_flag_csv
+	echo csv file: $_flag_csv
+	set csvRow 'TnG_Tracker,TnG_TrackTime,TnG_Value,TnG_Label,TnG_Note'
+	if not test $_flag_dry_run
+		echo $csvRow > $_flag_csv
+	else
+		echo $csvRow
+	end
+else
+	set NotePath $DefaultPath
+	set RootNote (obsidian search query='["TnG_ROOT":true]' | string collect)
+	if test (echo $RootNote | wc -l) -gt 1
+		echo 'Error: ["TnG_ROOT":true] defined more than once in the vault.'
+		return 1
+	end
+	if test $RootNote != "No matches found."
+		set NotePath (dirname $RootNote)
+	end
 
-set VaultRoot (obsidian vault info=path)
+	set VaultRoot (obsidian vault info=path)
+end
 
 echo $SQLquery | sqlite3 $TrackAndGraphBackup_db |
 while read ResultLine
-	echo $ResultLine
 	set ResultFields (string split "|" $ResultLine)
 	set TnG_Tracker    $ResultFields[1]
 	set TnG_TrackTime  $ResultFields[2]
@@ -141,21 +158,32 @@ while read ResultLine
 	set TnG_Note       $ResultFields[5]
 	set NoteName       $ResultFields[6]
 
-	if not test $_flag_dry_run
-		if not test -f "$VaultRoot/$NotePath/$NoteName.md"
-			obsidian create path="$NotePath/$NoteName.md" < /dev/null
+	if test $_flag_csv
+		set csvRow '"'$TnG_Tracker'",'$TnG_TrackTime','$TnG_Value',"'$TnG_Label'","'$TnG_Note'"'
+		if not test $_flag_dry_run
+			echo $csvRow >> $_flag_csv
+		else
+			echo $csvRow
 		end
-		obsidian property:set name="TnG_Tracker"   value="$TnG_Tracker"   type=text     path="$NotePath/$NoteName.md" < /dev/null
-		obsidian property:set name="TnG_TrackTime" value="$TnG_TrackTime" type=datetime path="$NotePath/$NoteName.md" < /dev/null
-		obsidian property:set name="TnG_Value"     value="$TnG_Value"     type=number   path="$NotePath/$NoteName.md" < /dev/null
-		obsidian property:set name="TnG_Label"     value="$TnG_Label"     type=text     path="$NotePath/$NoteName.md" < /dev/null
-		obsidian property:set name="TnG_Note"      value="$TnG_Note"      type=text     path="$NotePath/$NoteName.md" < /dev/null
 	else
-		echo "create path='$NotePath/$NoteName.md'"
-		echo "property:set name='TnG_Tracker' value='$TnG_Tracker' type=text path='$NotePath/$NoteName.md'"
-		echo "property:set name='TnG_TrackTime' value='$TnG_TrackTime' type=datetime path='$NotePath/$NoteName.md'"
-		echo "property:set name='TnG_Value' value='$TnG_Value' type=number path='$NotePath/$NoteName.md'"
-		echo "property:set name='TnG_Label' value='$TnG_Label' type=text path='$NotePath/$NoteName.md'"
-		echo "property:set name='TnG_Note' value='$TnG_Note' type=text path='$NotePath/$NoteName.md'"
+		begin
+		if not test $_flag_dry_run
+			if not test -f "$VaultRoot/$NotePath/$NoteName.md"
+				obsidian create path="$NotePath/$NoteName.md" < /dev/null
+			end
+			obsidian property:set name="TnG_Tracker"   value="$TnG_Tracker"   type=text     path="$NotePath/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_TrackTime" value="$TnG_TrackTime" type=datetime path="$NotePath/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_Value"     value="$TnG_Value"     type=number   path="$NotePath/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_Label"     value="$TnG_Label"     type=text     path="$NotePath/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_Note"      value="$TnG_Note"      type=text     path="$NotePath/$NoteName.md" < /dev/null
+		else
+			echo "create path='$NotePath/$NoteName.md'"
+			echo "property:set name='TnG_Tracker' value='$TnG_Tracker' type=text path='$NotePath/$NoteName.md'"
+			echo "property:set name='TnG_TrackTime' value='$TnG_TrackTime' type=datetime path='$NotePath/$NoteName.md'"
+			echo "property:set name='TnG_Value' value='$TnG_Value' type=number path='$NotePath/$NoteName.md'"
+			echo "property:set name='TnG_Label' value='$TnG_Label' type=text path='$NotePath/$NoteName.md'"
+			echo "property:set name='TnG_Note' value='$TnG_Note' type=text path='$NotePath/$NoteName.md'"
+		end
+		end
 	end
 end
