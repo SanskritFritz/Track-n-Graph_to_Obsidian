4b54ca8f6a0485253c2f9c6a116940cb30afccd9
SanskritFritz <>
2026-04-03 17:25:02 +0200

    Better error handling for comman line options
    Refactoring variable names, etc

--- Track-n-Graph_to_Obsidian.sh
+++ Track-n-Graph_to_Obsidian.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Import [Track & Graph](https://github.com/SamAmco/track-and-graph) data to Obsidian or CSV file.
 # Generate notes with properties, one for each data point.
@@ -21,82 +21,137 @@
 # TODO
 # • Also filter for labels and notes
 # • Error detection for Obsidian calls
-# • Prevent accidental run by requiring an option
 # • Make sure Obsidian GUI is already running
+# • set @bind_variable 'value': SELECT * FROM mytable WHERE field = @bind_variable;
 
 # Defaults:
 TrackAndGraphBackup_db="TrackAndGraphBackup.db"
-NotePath="Data/Track-n-Graph"
+note_path="Data/Track-n-Graph"
 
 # Parse arguments
-database=""
+database_file=""
 declare -a trackers
 min_time=""
 max_time=""
-dry_run=false
 csv_file=""
+dry_run=false
 help=false
+error_happened=false
 
 while [[ $# -gt 0 ]]; do
 	case $1 in
 		-d=*|--database=*)
-			database="${1#*=}"
+			database_file="${1#*=}"
+			if [[ -z "$database_file" ]]; then
+				# no value given
+				database_file="$TrackAndGraphBackup_db"
+			fi
 			shift 1
 			;;
 		-d|--database)
-			database="$2"
-			shift 2
+			if [[ -n "$2" && "$2" != -* ]]; then
+				database_file="$2"
+				shift 1
+			else
+				# no value given
+				database_file="$TrackAndGraphBackup_db"
+			fi
+			shift 1
 			;;
 		-t=*|--tracker=*)
-			trackers+=("${1#*=}")
+			tracker="${1#*=}"
+			if [[ -n "$tracker" ]]; then
+				# -t|--tracker can be given more than once, hence the `trackers` array
+				trackers+=("$tracker")
+			else
+				echo "Error: value for -t|--tracker is mandatory!" >&2
+				error_happened=true
+			fi
 			shift 1
 			;;
 		-t|--tracker)
-			trackers+=("$2")
-			shift 2
+			if [[ -n "$2" && "$2" != -* ]]; then
+				trackers+=("$2")
+				shift 1
+			else
+				echo "Error: value for -t|--tracker is mandatory!" >&2
+				error_happened=true
+			fi
+			shift 1
 			;;
 		--min-time=*)
 			min_time="${1#*=}"
+			if [[ -z "$min_time" ]]; then
+				echo "Error: value for --min-time is mandatory!" >&2
+				error_happened=true
+			fi
 			shift 1
 			;;
 		--min-time)
-			min_time="$2"
-			shift 2
+			if [[ -n "$2" && "$2" != -* ]]; then
+				min_time="$2"
+				shift 1
+			else
+				echo "Error: value for --min-time is mandatory!" >&2
+				error_happened=true
+			fi
+			shift 1
 			;;
 		--max-time=*)
 			max_time="${1#*=}"
+			if [[ -z "$min_time" ]]; then
+				echo "Error: value for --max-time is mandatory!" >&2
+				error_happened=true
+			fi
 			shift 1
 			;;
 		--max-time)
-			max_time="$2"
-			shift 2
+			if [[ -n "$2" && "$2" != -* ]]; then
+				max_time="$2"
+				shift 1
+			else
+				echo "Error: value for --max-time is mandatory!" >&2
+				error_happened=true
+			fi
+			shift 1
 			;;
 		--csv=*)
 			csv_file="${1#*=}"
+			if [[ -z "$csv_file" ]]; then
+				echo "Error: value for --csv is mandatory!" >&2
+				error_happened=true
+			fi
 			shift 1
 			;;
 		--csv)
-			csv_file="$2"
-			shift 2
+			if [[ -n "$2" && "$2" != -* ]]; then
+				csv_file="$2"
+				shift 1
+			else
+				echo "Error: value for --csv is mandatory!" >&2
+				error_happened=true
+			fi
+			shift 1
 			;;
 		--dry-run)
 			dry_run=true
-			shift
+			shift 1
 			;;
 		-h|--help)
 			help=true
-			shift
+			shift 1
 			;;
 		*)
-			echo "Unknown option: $1"
-			exit 1
+			echo "Error: unknown option: '$1'!" >&2
+			error_happened=true
+			shift 1
 			;;
 	esac
 done
 
 if [[ "$help" == true ]]; then
 	echo "Usage:"
-	echo "$(basename "$0") \\"
+	echo "$0 \\"
 	echo "    [-d|--database=<path/to/TrackAndGraphBackup.db>] \\"
 	echo "    [-t|--tracker=<tracker> [...]] \\"
 	echo "    [--min-time=<datetime>] [--max-time=<datetime>] \\"
@@ -105,12 +160,14 @@ if [[ "$help" == true ]]; then
 	echo "    [-h|--help]"
 	echo
 	echo "Example:"
-	echo "$(basename "$0") \\"
+	echo "$0 \\"
 	echo "    --database='/home/user/backup/android/TrackAndGraphBackup.db' \\"
 	echo "    --tracker='Meat' --tracker='Vegetables' \\"
 	echo "    --min-time='-3 days 0' --max-time='today 0'"
 	echo
-	echo "Database name defaults to 'TrackAndGraphBackup.db'."
+	echo "-d or --database is mandatory (this prevents accidental run)"
+	echo "If no value was given, the name defaults to '$TrackAndGraphBackup_db'."
+	echo
 	echo "Min/max time values are validated with 'date -d'."
 	echo
 	echo "Obsidian path is determined by the note property 'TnG_ROOT: true'"
@@ -122,77 +179,70 @@ if [[ "$help" == true ]]; then
 	exit 0
 fi
 
-ValueError=0
-
-if [[ -n "$database" ]]; then
-	TrackAndGraphBackup_db="$database"
-fi
-
-if [[ ! -f "$TrackAndGraphBackup_db" ]]; then
-	echo "Error: unable to find '$TrackAndGraphBackup_db'!" >&2
-	ValueError=1
+if [[ -z "$database_file" ]]; then
+	echo "Error: -d or --database is mandatory! This prevents accidental run." >&2
+	error_happened=true
+elif [[ ! -f "$database_file" ]]; then
+	echo "Error: unable to find database '$database_file'!" >&2
+	error_happened=true
 fi
 
 if [[ -n "$csv_file" ]] && [[ "$dry_run" != true ]]; then
 	if ! touch "$csv_file" 2>/dev/null; then
 		echo "Error: invalid csv file name: '$csv_file'!" >&2
-		ValueError=1
+		error_happened=true
 	fi
 fi
 
 # Build tracker SQL clause
-TrackersProvided=0
-TrackersSQL=""
-Trackers=""
+trackers_SQL=""
+trackers_list=""
 
 for tracker in "${trackers[@]}"; do
 	if [[ $tracker =~ ^[a-zA-Z0-9_]+$ ]]; then
-		Trackers="$Trackers,'$tracker'"
+		trackers_list="$trackers_list,'$tracker'"
 	else
 		echo "Error: the value '$tracker' is not allowed for -t|--tracker!" >&2
-		ValueError=1
+		error_happened=true
 	fi
-	TrackersProvided=1
 done
 
-if [[ $TrackersProvided -eq 1 ]]; then
-	TrackersSQL="AND (tracker in (${Trackers:1}))"
+if [[ -n $trackers_list ]]; then
+	trackers_SQL="AND (tracker in (${trackers_list:1}))"
 fi
 
 # Build min time SQL clause
-MinTime=""
-MinTimeSQL=""
+min_time_SQL=""
 if [[ -n "$min_time" ]]; then
-	MinTime=$(date -d "$min_time" +'%Y-%m-%dT%H:%M:%S' 2>/dev/null)
-	if [[ "$MinTime" ]]; then
-		MinTimeSQL="AND (track_time >= '$MinTime')"
+	min_time_ISO=$(date -d "$min_time" +'%Y-%m-%dT%H:%M:%S' 2>/dev/null)
+	if [[ -n "$min_time_ISO" ]]; then
+		min_time_SQL="AND (track_time >= '$min_time_ISO')"
 	else
 		echo "Error: the value '$min_time' is not allowed for --min-time!" >&2
-		ValueError=1
+		error_happened=true
 	fi
 fi
 
 # Build max time SQL clause
-MaxTime=""
-MaxTimeSQL=""
+max_time_SQL=""
 if [[ -n "$max_time" ]]; then
-	MaxTime=$(date -d "$max_time" +'%Y-%m-%dT%H:%M:%S' 2>/dev/null)
-	if [[ "$MaxTime" ]]; then
-		MaxTimeSQL="AND (track_time <= '$MaxTime')"
+	max_time_ISO=$(date -d "$max_time" +'%Y-%m-%dT%H:%M:%S' 2>/dev/null)
+	if [[ "$max_time_ISO" ]]; then
+		max_time_SQL="AND (track_time <= '$max_time_ISO')"
 	else
 		echo "Error: the value '$max_time' is not allowed for --max-time!" >&2
-		ValueError=1
+		error_happened=true
 	fi
 fi
 
 # Exit if any error occurred
-if [[ $ValueError -eq 1 ]]; then
+if [[ $error_happened -eq 1 ]]; then
 	exit 1
 fi
 
 # The checks above made sure all parameters are SQL-injection-safe
-read -r -d '' SQLquery << 'EOF'
-SELECT tracker, track_time, value, label, note, file_name FROM (
+SQLquery="
+SELECT tracker, track_time, value, label, note, note_name FROM (
 	SELECT
 		f.name as tracker,
 		dp.epoch_milli as epoch,
@@ -200,16 +250,15 @@ SELECT tracker, track_time, value, label, note, file_name FROM (
 		dp.value,
 		dp.label,
 		dp.note,
-		strftime('%Y-%m-%d %H.%M.%S', DATETIME(ROUND(dp.epoch_milli/1000), 'unixepoch')) as file_name
+		strftime('%Y-%m-%d %H.%M.%S', DATETIME(ROUND(dp.epoch_milli/1000), 'unixepoch')) as note_name
 	FROM data_points_table dp
 	JOIN features_table f on dp.feature_id = f.id
 )
 WHERE 1=1
-	$TrackersSQL
-	$MinTimeSQL
-	$MaxTimeSQL
-ORDER BY epoch desc
-EOF
+	$trackers_SQL
+	$min_time_SQL
+	$max_time_SQL
+ORDER BY epoch desc"
 
 if [[ "$dry_run" == true ]]; then
 	echo "-- DRY RUN --"
@@ -217,55 +266,55 @@ if [[ "$dry_run" == true ]]; then
 fi
 
 if [[ -n "$csv_file" ]]; then
-	echo "csv file: $csv_file"
-	csvRow='TnG_Tracker,TnG_TrackTime,TnG_Value,TnG_Label,TnG_Note'
+	echo "CSV file: $csv_file"
+	csv_row='TnG_Tracker,TnG_TrackTime,TnG_Value,TnG_Label,TnG_Note'
 	if [[ "$dry_run" != true ]]; then
-		echo "$csvRow" > "$csv_file"
+		echo "$csv_row" > "$csv_file"
 	else
-		echo "$csvRow"
+		echo "$csv_row"
 	fi
 else
-	RootNote=$(obsidian search query='["TnG_ROOT":true]' | tr '\n' ' ')
-	RootNoteLineCount=$(echo "$RootNote" | wc -l)
+	root_note=$(obsidian search query='["TnG_ROOT":true]')
+	root_notes_count=$(echo "$root_note" | wc -l)
 
-	if [[ $RootNoteLineCount -gt 1 ]]; then
+	if [[ $root_notes_count -gt 1 ]]; then
 		echo 'Error: ["TnG_ROOT":true] defined more than once in the vault.' >&2
 		exit 1
 	fi
 
-	if [[ "$RootNote" != "No matches found." ]]; then
-		NotePath=$(dirname "$RootNote")
+	if [[ "$root_note" != "No matches found." ]]; then
+		note_path=$(dirname "$root_note")
 	fi
 
-	VaultRoot=$(obsidian vault info=path)
+	vaultroot_path=$(obsidian vault info=path)
 fi
 
 echo "$SQLquery" | sqlite3 "$TrackAndGraphBackup_db" |
 while IFS='|' read -r TnG_Tracker TnG_TrackTime TnG_Value TnG_Label TnG_Note NoteName; do
 	if [[ -n "$csv_file" ]]; then
-		csvRow="\"$TnG_Tracker\",$TnG_TrackTime,$TnG_Value,\"$TnG_Label\",\"$TnG_Note\""
+		csv_row="\"$TnG_Tracker\",$TnG_TrackTime,$TnG_Value,\"$TnG_Label\",\"$TnG_Note\""
 		if [[ "$dry_run" != true ]]; then
-			echo "$csvRow" >> "$csv_file"
+			echo "$csv_row" >> "$csv_file"
 		else
-			echo "$csvRow"
+			echo "$csv_row"
 		fi
 	else
 		if [[ "$dry_run" != true ]]; then
-			if [[ ! -f "$VaultRoot/$NotePath/$NoteName.md" ]]; then
-				obsidian create path="$NotePath/$NoteName.md" < /dev/null
+			if [[ ! -f "$vaultroot_path/$note_path/$NoteName.md" ]]; then
+				obsidian create path="$note_path/$NoteName.md" < /dev/null
 			fi
-			obsidian property:set name="TnG_Tracker"   value="$TnG_Tracker"   type=text     path="$NotePath/$NoteName.md" < /dev/null
-			obsidian property:set name="TnG_TrackTime" value="$TnG_TrackTime" type=datetime path="$NotePath/$NoteName.md" < /dev/null
-			obsidian property:set name="TnG_Value"     value="$TnG_Value"     type=number   path="$NotePath/$NoteName.md" < /dev/null
-			obsidian property:set name="TnG_Label"     value="$TnG_Label"     type=text     path="$NotePath/$NoteName.md" < /dev/null
-			obsidian property:set name="TnG_Note"      value="$TnG_Note"      type=text     path="$NotePath/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_Tracker"   value="$TnG_Tracker"   type=text     path="$note_path/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_TrackTime" value="$TnG_TrackTime" type=datetime path="$note_path/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_Value"     value="$TnG_Value"     type=number   path="$note_path/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_Label"     value="$TnG_Label"     type=text     path="$note_path/$NoteName.md" < /dev/null
+			obsidian property:set name="TnG_Note"      value="$TnG_Note"      type=text     path="$note_path/$NoteName.md" < /dev/null
 		else
-			echo "create path='$NotePath/$NoteName.md'"
-			echo "property:set name='TnG_Tracker' value='$TnG_Tracker' type=text path='$NotePath/$NoteName.md'"
-			echo "property:set name='TnG_TrackTime' value='$TnG_TrackTime' type=datetime path='$NotePath/$NoteName.md'"
-			echo "property:set name='TnG_Value' value='$TnG_Value' type=number path='$NotePath/$NoteName.md'"
-			echo "property:set name='TnG_Label' value='$TnG_Label' type=text path='$NotePath/$NoteName.md'"
-			echo "property:set name='TnG_Note' value='$TnG_Note' type=text path='$NotePath/$NoteName.md'"
+			echo "create path='$note_path/$NoteName.md'"
+			echo "property:set name='TnG_Tracker' value='$TnG_Tracker' type=text path='$note_path/$NoteName.md'"
+			echo "property:set name='TnG_TrackTime' value='$TnG_TrackTime' type=datetime path='$note_path/$NoteName.md'"
+			echo "property:set name='TnG_Value' value='$TnG_Value' type=number path='$note_path/$NoteName.md'"
+			echo "property:set name='TnG_Label' value='$TnG_Label' type=text path='$note_path/$NoteName.md'"
+			echo "property:set name='TnG_Note' value='$TnG_Note' type=text path='$note_path/$NoteName.md'"
 		fi
 	fi
 done
